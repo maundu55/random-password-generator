34f3a6b1254e4fd61c1a07f87ef3c7897be521b9
maundu55 <marymaundu.mm@gmail.com>
2024-09-19 08:47:51 +0300

    project complete

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const passwordEl = document.getElementById('password');
-const buttonEl = document.getElementById('btn');
+const passwordEl = document.getElementById("password");
+const buttonEl = document.getElementById("btn");
 
-const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789,.;:?-_*&%$#@!^/'
-let passLength = 8;
-let password = '';
+buttonEl.addEventListener("click", generatePass);
 
-buttonEl.addEventListener('click', generatePass)
+function generatePass() {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789,.;:?-_*&%$#@!^/";
+  let passLength = 8;
+  let password = "";
 
-function generatePass(){
-
-  for(i = 0; i <= passLength; i++){
+  for (i = 0; i <= passLength; i++) {
     let randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
   }
   passwordEl.value = password;
-  console.log(password);
- 
 }
 const copyBtn = document.getElementById("copy");
 copyBtn.addEventListener("click", () => {
   passwordEl.select();
   document.execCommand("copy");
   alert("Password Copied");
-//   passwordEl.value = '';
 });
-
